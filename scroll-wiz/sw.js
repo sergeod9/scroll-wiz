dc8e7b552938c394782033f9386c9cb4d62d5f1b
Georges Dahdouh <georges.dahdouh@gmail.com>
2022-08-22 22:48:47 +0300

    Update sw.js

--- scroll-wiz/sw.js
+++ scroll-wiz/sw.js
@@ -48,27 +48,3 @@ function swRun(arr){
         }
     }  
 }
-// updateElements(arr)
-
-// document.addEventListener('scroll', e =>{
-//     pageScrollY = window.scrollY
-//     console.log(pageScrollY)
-//     window.requestAnimationFrame(()=>{
-//         updateElements(arr)
-//     })
-// })
-
-// function updateElements(arr){
-//     for (let item of arr){
-//         if (item.min <= pageScrollY){
-//             item.el.classList.add(item.cls)
-//         } 
-//         else{
-//             item.el.classList.remove(item.cls)
-//         }
-    
-//         if (item.max <= pageScrollY){
-//             item.el.classList.remove(item.cls)
-//         }
-//     }
-// }
